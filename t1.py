ccacb0d36a554ebfe5789fa5921d727e229565f0
Abhilashg11 <alexrog1122@gmail.com>
2023-02-09 16:22:40 +0000

    car and bike example using init

---
+++ t1.py
@@ -0,0 +1,16 @@
+class i:
+    def __init__(self,car,bike):
+        self.c=car
+        self.b=bike
+    def speed(self):
+        print(self.c,self.b)   
+
+o1=i("mustang","r1")
+o2=i("lambo","r6")
+
+o1.speed()
+o2.speed()
+
+
+
+
